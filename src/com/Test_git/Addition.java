c609e3e03396801dc024ce785f9ceb7f67168eb3
ShamailOmar <shama.omer@gmail.com>
2024-09-02 15:30:01 +0530

    new class file updated

---
+++ src/com/Test_git/Addition.java
@@ -0,0 +1,19 @@
+package com.Test_git;
+
+public class Addition {
+	
+	public void add() {
+		int a=10;
+		int b=10;
+		int c;
+		c=a+b;
+		System.out.println("sum="+c);		
+	}
+	
+	public static void main(String[] args) {
+		Addition a = new Addition();
+		a.add();
+		
+	}
+
+}
